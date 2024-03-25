8bcf30bd8a85445d683dcb7acf7d56760ced4d91
Nathaniel <ndm0113@gmail.com>
2024-03-24 20:26:34 -0400

    Solve Common Errors

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,19 @@
     let titleContains: string = "";
     let valueContains: string = "";
     let keyContains: string = "";
+    let empty: boolean = false;
+    let noDescription = false;
 
-    $: filtered = data.filter(c =>
-        types.indexOf(c.type) > -1 &&
-        c.title.toLowerCase().indexOf(titleContains.toLowerCase()) > -1 &&
-        c.value.toLowerCase().indexOf(valueContains.toLowerCase()) > -1 &&
-        c.keys.split(',').filter(s => s.toLowerCase().indexOf(keyContains.toLowerCase()) > -1).length > 0);
+    $: filtered = data
+        .filter(c =>
+            types.indexOf(c.type) > -1 &&
+            c.title.toLowerCase().indexOf(titleContains.toLowerCase()) > -1 &&
+            c.value.toLowerCase().indexOf(valueContains.toLowerCase()) > -1 &&
+            c.keys.split(',').filter(s => s.toLowerCase().indexOf(keyContains.toLowerCase()) > -1).length > 0)
+        .filter(c =>
+            (!empty && !noDescription) ||
+            (empty && c.keys.split(',').filter(s => /^\s*$/gm.test(s)).length > 0) ||
+            (noDescription && (c.useForCharacterCreation == true && /^(\s*|Notes go here.)$/gm.test(c.description))));
 
     function resetFilter() {
         titleContains = valueContains = keyContains = "";
@@ -23,6 +30,7 @@
     }
 
     let files: FileList;
+
     async function fileUpdate() {
         if (files) {
             await files[0].text()
@@ -45,6 +53,7 @@
                 });
         }
     }
+
     function download() {
         const helper = document.createElement('a') as HTMLAnchorElement;
         const url = URL.createObjectURL(new Blob([JSON.stringify(data)], {type: "application/json"}));
@@ -60,6 +69,7 @@
         display: flex;
         flex-flow: column wrap;
         gap: 1ex;
+        width: 100%;
     }
 
     header {
@@ -106,11 +116,11 @@
     <section class="filter">
         <fieldset class="searchWithin">
             <label for="title">Title:</label>
-            <input name="title" bind:value={titleContains}/>
+            <input name="title" id="title" bind:value={titleContains}/>
             <label for="entry">Entry:</label>
-            <input name="entry" bind:value={valueContains}/>
+            <input name="entry" id="entry" bind:value={valueContains}/>
             <label for="triggers">Trigger:</label>
-            <input name="triggers" bind:value={keyContains}/>
+            <input name="triggers" id="triggers" bind:value={keyContains}/>
         </fieldset>
         <fieldset class="type">
             <label for="type">Type:</label>
@@ -123,9 +133,34 @@
         </fieldset>
         <button on:click={resetFilter}>Clear Filters</button>
     </section>
+    <small>Common Errors:</small>
+    <section class="issues">
+        <span>
+            <input title="Shows cards with empty or missing triggers. Empty triggers will cause cards to trigger constantly, while missing triggers will cause them to never trigger."
+                   type="checkbox" id="empty" name="empty" bind:checked={empty}/>
+            <label for="empty">Empty triggers</label>
+        </span>
+        <span>
+            <input title="Shows cards that are shown in character creator, but have no description. This only applies if you use character creator!"
+                   type="checkbox" id="noDescription" name="noDescription" bind:checked={noDescription}/>
+            <label for="noDescription">No Character Creator description</label>
+        </span>
+    </section>
 </nav>
 <main>
     {#each filtered as card}
         <StoryCard card={card}/>
+    {:else}
+        {#if data.length === 0}
+            <p>
+                Load some story cards to get started! Open a scenario or adventure, go to Edit > Details > Story Card
+                Management, and export your story cards! When you're done editing, you can save them here and import
+                them again in the same section.
+            </p>
+        {:else if empty || noDescription}
+            <p>No errors found! You're good!</p>
+        {:else}
+            <p>You've filtered everything out! To reset, click the "Clear Filters" button!</p>
+        {/if}
     {/each}
 </main>
