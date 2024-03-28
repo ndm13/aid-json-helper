89c2a21f0dc103c248aaf04ffe02c79ca1df3aaa
Nathaniel <ndm0113@gmail.com>
2024-03-27 22:18:54 -0400

    Initial refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
+    import { writable, derived } from 'svelte/store';
     import type {StoryCard as StoryCardType} from "./lib/model/StoryCard.ts";
     import StoryCard from "./lib/component/StoryCard.svelte";
     import Help from "./lib/component/Help.svelte";
 
-    let data: StoryCardType[] = [];
-
-    $: allTypes = [];
-    let types: string[] = [];
-    let titleContains: string = "";
-    let valueContains: string = "";
-    let keyContains: string = "";
-    let empty: boolean = false;
-    let noDescription = false;
-    let sort = "default";
-    let sortAsc = false;
-    let showTriggers = false;
-
-    function getTriggersDefaults() {
-        return getTriggers(data);
+    function getEmptyFilter() {
+        return structuredClone({
+            title: "",
+            value: "",
+            key: "",
+            types: $types,
+            empty: false,
+            noDescription: false,
+            sort: {
+                mode: "default",
+                asc: true
+            }
+        });
     }
 
-    function getTriggers(data) {
-        const triggers = data
+    export const cards = writable<StoryCardType>([]);
+
+    const triggers = derived(cards, cards => {
+        const triggers = cards
             .map(c => c.keys.split(','))
             .reduce((a, v) => {
                 v.forEach(t => a[t] ? a[t].count++ : a[t] = {count: 1});
@@ -29,46 +30,38 @@
             }, {});
         const keys = Object.keys(triggers);
         keys.forEach(t => triggers[t].overlap = keys.filter(k => k.indexOf(t) > -1 && t !== k).length);
-        return triggers;
-    }
+        return Object.entries(triggers).map(([k,v]) => { v.key = k; return v; });
+    });
 
-    $: triggers = getTriggers(data);
+    const types = derived(cards, cards => [...new Set(cards.map(c => c.type))]);
 
-    function getFilteredDefaults() {
-        return getFiltered(data, titleContains, valueContains, keyContains, empty, noDescription, sort, sortAsc);
-    }
+    const filter = writable(getEmptyFilter());
 
-    function getFiltered(data, titleContains, valueContains, keyContains, empty, noDescription, sort, sortAsc) {
-        let filtered = data
+    const filtered = derived([cards, filter], ([cards, filter]) => {
+        let filtered = cards
             .filter(c =>
-                types.indexOf(c.type) > -1 &&
-                c.title.toLowerCase().indexOf(titleContains.toLowerCase()) > -1 &&
-                c.value.toLowerCase().indexOf(valueContains.toLowerCase()) > -1 &&
-                c.keys.split(',').filter(s => s.toLowerCase().indexOf(keyContains.toLowerCase()) > -1).length > 0)
+                filter.types.indexOf(c.type) > -1 &&
+                c.title.toLowerCase().indexOf(filter.title.toLowerCase()) > -1 &&
+                c.value.toLowerCase().indexOf(filter.value.toLowerCase()) > -1 &&
+                c.keys.split(',').filter(s => s.toLowerCase().indexOf(filter.key.toLowerCase()) > -1).length > 0)
             .filter(c =>
-                (!empty && !noDescription) ||
-                (empty && c.keys.split(',').filter(s => /^\s*$/gm.test(s)).length > 0) ||
-                (noDescription && (c.useForCharacterCreation == true && /^(\s*|Notes go here.)$/gm.test(c.description))));
-        return sort === "default" ? filtered : filtered
+                (!filter.empty && !filter.noDescription) ||
+                (filter.empty && c.keys.split(',').filter(s => /^\s*$/gm.test(s)).length > 0) ||
+                (filter.noDescription && (c.useForCharacterCreation == true && /^(\s*|Notes go here.)$/gm.test(c.description))));
+        return filter.sort.mode === "default" ? filtered : filtered
             .sort((a, b) => {
-                switch (sort) {
+                switch (filter.sort.mode) {
                     case "alpha":
-                        return (sortAsc ? a.title.toLowerCase() < b.title.toLowerCase() : b.title.toLowerCase() < a.title.toLowerCase()) ? 1 : -1;
+                        return (filter.sort.asc ? a.title.toLowerCase() < b.title.toLowerCase() : b.title.toLowerCase() < a.title.toLowerCase()) ? 1 : -1;
                     case "type":
-                        return (sortAsc ? a.type.toLowerCase() < b.type.toLowerCase() : b.type.toLowerCase() < a.type.toLowerCase()) ? 1 : -1;
+                        return (filter.sort.asc ? a.type.toLowerCase() < b.type.toLowerCase() : b.type.toLowerCase() < a.type.toLowerCase()) ? 1 : -1;
                     case "length":
-                        return !sortAsc ? a.value.length - b.value.length : b.value.length - a.value.length;
+                        return !filter.sort.asc ? a.value.length - b.value.length : b.value.length - a.value.length;
                 }
             });
-    }
+    });
 
-    $: filtered = getFiltered(data, titleContains, valueContains, keyContains, empty, noDescription, sort, sortAsc);
-
-    function resetFilter() {
-        titleContains = valueContains = keyContains = "";
-        types.length = 0;
-        types.push(...allTypes);
-    }
+    let showTriggers = false;
 
     let files: FileList;
 
@@ -77,18 +70,14 @@
             await files[0].text()
                 .then(t => JSON.parse(t))
                 .then(c => {
-                    data.length = 0;
-                    data.push(...c);
-                    allTypes.length = 0;
-                    allTypes.push(...new Set(data.map(c => c.type)));
-                    resetFilter();
-                    if (data.length === 0)
+                    $cards = c;
+                    $filter = getEmptyFilter();
+                    if ($cards.length === 0)
                         alert("This file doesn't contain any story cards!  Make sure you have the right file.");
                 })
                 .catch(e => {
                     // Destroy the current session anyway to keep from saving under the wrong name
-                    data.length = 0;
-                    resetFilter();
+                    $cards = [];
                     alert("There was an error trying to load this file.  Are you sure it's valid?");
                     console.error("Caught error when updating file", e);
                 });
@@ -97,7 +86,7 @@
 
     function download() {
         const helper = document.createElement('a') as HTMLAnchorElement;
-        const url = URL.createObjectURL(new Blob([JSON.stringify(data)], {type: "application/json"}));
+        const url = URL.createObjectURL(new Blob([JSON.stringify($cards)], {type: "application/json"}));
         helper.download = files ? files[0].name : "cards.json";
         helper.href = url;
         helper.click();
@@ -194,89 +183,85 @@
     </section>
 </header>
 <nav>
-    <em>Showing {filtered.length} entries out of {data.length}</em>
+    <em>Showing {$filtered.length} entries out of {$cards.length}</em>
     <section class="filter">
         <fieldset class="searchWithin">
             <label for="title">Title:</label>
-            <input name="title" id="title" bind:value={titleContains}/>
+            <input name="title" id="title" bind:value={$filter.title}/>
             <label for="entry">Entry:</label>
-            <input name="entry" id="entry" bind:value={valueContains}/>
+            <input name="entry" id="entry" bind:value={$filter.value}/>
             <label for="triggers">Trigger:</label>
-            <input name="triggers" id="triggers" bind:value={keyContains}/>
+            <input name="triggers" id="triggers" bind:value={$filter.key}/>
         </fieldset>
         <fieldset class="type">
             <label for="type">Type:</label>
-            <select name="type" multiple bind:value={types}>
-                {#each allTypes as t}
+            <select name="type" multiple bind:value={$filter.types}>
+                {#each $types as t}
                     <option selected>{t}</option>
                 {/each}
             </select>
-            <button on:click={()=>types.length = 0}>Clear Types</button>
+            <button on:click={() => $filter.types.length = 0}>Clear Types</button>
         </fieldset>
-        <button on:click={resetFilter}>Clear Filters</button>
+        <button on:click={() => $filter = getEmptyFilter()}>Clear Filters</button>
     </section>
     <small>Sort:</small>
     <section class="sort">
         <span>
-            <input type="radio" id="alpha" name="sort" value="alpha" bind:group={sort}/>
+            <input type="radio" id="alpha" name="sort" value="alpha" bind:group={$filter.sort.mode}/>
             <label for="alpha">Alphabetically</label>
         </span>
         <span>
-            <input type="radio" id="type" name="sort" value="type" bind:group={sort}/>
+            <input type="radio" id="type" name="sort" value="type" bind:group={$filter.sort.mode}/>
             <label for="type">By type</label>
         </span>
         <span>
-            <input type="radio" id="length" name="sort" value="length" bind:group={sort}/>
+            <input type="radio" id="length" name="sort" value="length" bind:group={$filter.sort.mode}/>
             <label for="length">By entry length</label>
         </span>
-        <button on:click={() => {sortAsc = !sortAsc;filtered = getFilteredDefaults();}}>
-            Sort {sortAsc ? "Descending" : "Ascending"}</button>
+        <button on:click={() => $filter.sort.asc = !$filter.sort.asc}>
+            Sort {$filter.sort.asc ? "Descending" : "Ascending"}</button>
     </section>
     <small>Common Errors:</small>
     <section class="issues">
         <span>
             <input title="Shows cards with empty or missing triggers. Empty triggers will cause cards to trigger constantly, while missing triggers will cause them to never trigger."
-                   type="checkbox" id="empty" name="empty" bind:checked={empty}/>
+                   type="checkbox" id="empty" name="empty" bind:checked={$filter.empty}/>
             <label for="empty">Empty triggers</label>
         </span>
         <span>
             <input title="Shows cards that are shown in character creator, but have no description. This only applies if you use character creator!"
-                   type="checkbox" id="noDescription" name="noDescription" bind:checked={noDescription}/>
+                   type="checkbox" id="noDescription" name="noDescription" bind:checked={$filter.noDescription}/>
             <label for="noDescription">No Character Creator description</label>
         </span>
     </section>
     <small>Triggers:</small>
     <section class="triggers">
         {#if showTriggers}
-            {#each Object.keys(triggers) as trigger}
-                <kbd on:click={() => {resetFilter();keyContains = trigger;}}
-                     class:plural={triggers[trigger].count > 1}
-                     class:overlap={triggers[trigger].overlap > 0}>
-                    {trigger}{triggers[trigger].count > 1 ? " x" + triggers[trigger].count : ""}{triggers[trigger].overlap > 0 ? " +" + triggers[trigger].overlap : ""}</kbd>
+            {#each $triggers as trigger}
+                <kbd on:click={() => {$filter = getEmptyFilter(); trigger.key === "" ? $filter.empty = true : $filter.key = trigger.key;}}
+                     class:plural={trigger.count > 1}
+                     class:overlap={trigger.overlap > 0}>
+                    {trigger.key}{trigger.count > 1 ? " x" + trigger.count : ""}{trigger.overlap > 0 ? " +" + trigger.overlap : ""}</kbd>
             {/each}
         {/if}
     </section>
     <span>
         <button on:click={() => showTriggers = !showTriggers}>
-            {showTriggers ? "Hide" : "Show"} Triggers ({Object.keys(triggers).length})
+            {showTriggers ? "Hide" : "Show"} Triggers ({Object.keys($triggers).length})
         </button>
     </span>
 </nav>
 <main>
-    {#each filtered as card}
-        <StoryCard card={card}
-                   on:editTriggers={() => triggers = getTriggersDefaults()}
-                   on:editTitle={() => filtered = getFilteredDefaults()}
-                   on:editType={() => filtered = getFilteredDefaults()}
-                   on:editValue={() => filtered = getFilteredDefaults()}/>
+    {#each $filtered as card}
+        <StoryCard card={card}/>
     {:else}
-        {#if data.length === 0}
+        {#if $cards.length === 0}
             <p>
                 Load some story cards to get started! Open a scenario or adventure, go to Edit > Details > Story Card
                 Management, and export your story cards! When you're done editing, you can save them here and import
                 them again in the same section.
             </p>
-        {:else if empty || noDescription}
+        {:else if $filter.empty || $filter.noDescription}
             <p>No errors found! You're good!</p>
         {:else}
             <p>You've filtered everything out! To reset, click the "Clear Filters" button!</p>
